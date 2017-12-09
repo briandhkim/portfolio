4ff126e0576ecc91092f6ee504e976e4b2653241
briandhkim <w1057216@gmail.com>
2017-12-08 19:27:09 -0800

    auto reply message edit

--- mail/mail_handler.php
+++ mail/mail_handler.php
@@ -95,7 +95,7 @@ echo json_encode($output);
 $mail->clearAddresses();
 $mail->clearReplyTos();
 $mail->clearCCs();
-$auto_reply = 'Thank you for reaching out! I will get back to you as soon as possible. Text to 949)285-1898 for any urgent issues.';
+$auto_reply = "Hello! Thank you for reaching out! I will get back to you as soon as possible. \n Text to 949)285-1898 for any urgent issues. You can also email to w1057216@g.ucla.edu. \n Thank you";
 $mail->FromName = 'Brian Kim';
 $mail->Subject = 'Thank you for reaching out!';
 $mail->Body = $auto_reply;
